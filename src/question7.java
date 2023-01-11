934b9504f5ea99b83eccd848d3ace30edbb920bd
BurakMavi <cryptomavi@outlook.com>
2023-01-11 17:56:52 -0500

    Question 7 files created

---
+++ src/question7.java
@@ -0,0 +1,12 @@
+package src;
+
+public class question7 {
+
+    public static void main(String[] args) {
+
+        /*
+        7. Numbers -- Prime NumberWrite a method that can check if a number is prime or not
+         */
+
+    }
+}
