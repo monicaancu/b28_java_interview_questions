0c3a985fe3e4f02ab1f3e678d833d59e65152c46
BurakMavi <cryptomavi@outlook.com>
2023-01-12 18:20:45 -0500

    Question 7 last updated

--- src/question7/question7.java
+++ src/question7/question7.java
@@ -10,15 +10,15 @@ public class question7 {
 
 
                 int num = 31;
-                boolean primeNum = false;
+                boolean Num = false;
                 for (int i = 2; i <= num / 2; ++i) {
                     if (num % i == 0) {
-                        primeNum = true;
+                        Num = true;
                         break;
                     }
                 }
 
-                if (!primeNum)
+                if (!Num)
                     System.out.println(num + " is a prime number.");
                 else
                     System.out.println(num + " is not a prime number.");
