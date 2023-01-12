6dcaf2c84e12f48e9f68cb9b44ace6ba8262bf80
BurakMavi <cryptomavi@outlook.com>
2023-01-12 15:43:12 -0500

    Question 7 codes are done

--- src/question7.java
+++ src/question7.java
@@ -8,5 +8,21 @@ public class question7 {
         7. Numbers -- Prime NumberWrite a method that can check if a number is prime or not
          */
 
+
+                int num = 31;
+                boolean primeNum = false;
+                for (int i = 2; i <= num / 2; ++i) {
+                    if (num % i == 0) {
+                        primeNum = true;
+                        break;
+                    }
+                }
+
+                if (!primeNum)
+                    System.out.println(num + " is a prime number.");
+                else
+                    System.out.println(num + " is not a prime number.");
+
+
     }
 }
